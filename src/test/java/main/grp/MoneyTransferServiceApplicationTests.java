ddca0233c6d65b4b0b53b0d6733b46f92193b7e4
KomX <komx512@outlook.com>
2025-10-23 22:05:57 +0500

    first commit

---
+++ src/test/java/main/grp/MoneyTransferServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package main.grp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyTransferServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
